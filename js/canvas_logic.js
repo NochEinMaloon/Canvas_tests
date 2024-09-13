399e6b80d313859cd4c5032bebd38941fb0e5a6a
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-13 17:10:30 +0200

    crorgfds

--- js/canvas_logic.js
+++ js/canvas_logic.js
@@ -19,7 +19,7 @@ function init(){
     canvas.appendChild(renderer.domElement); */
 
     // Einfache Geometrie hinzufügen
-    const geometry = new THREE.BoxGeometry();
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);

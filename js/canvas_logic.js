efed2a34be3b4a10a8ded4993e7415dbe21adf60
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-13 16:35:09 +0200

    rendamed canvas

--- js/canvas_logic.js
+++ js/canvas_logic.js
@@ -2,7 +2,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('canvas-container').appendChild(renderer.domElement);
+document.getElementById('canvas').appendChild(renderer.domElement);
 
 // Einfache Geometrie hinzufügen
 const geometry = new THREE.BoxGeometry();

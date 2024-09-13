6836b566e6994424af6e4167723de17aabd3d195
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-13 17:33:26 +0200

    replaced color

--- js/canvas_logic.js
+++ js/canvas_logic.js
@@ -33,7 +33,7 @@ function init(){
 
 function Spawncube(){
     const geometry = new THREE.BoxGeometry(5, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: ff0000 });
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 

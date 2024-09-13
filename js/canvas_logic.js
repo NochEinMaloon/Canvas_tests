f10aa1c2b69dca9421502121fe552621ef86871b
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-13 17:12:27 +0200

    added DOM back lol

--- js/canvas_logic.js
+++ js/canvas_logic.js
@@ -1,5 +1,5 @@
 //define constants
-const CamvasContainerElement = 
+const CamvasContainerElement = document.getElementById('canvas-container');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);

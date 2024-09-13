8168394033ddf07da9630e577de3eaa4a61a6834
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-13 17:30:19 +0200

    cube red

--- js/canvas_logic.js
+++ js/canvas_logic.js
@@ -33,9 +33,16 @@ function init(){
 
 function Spawncube(){
     const geometry = new THREE.BoxGeometry(5, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshBasicMaterial({ color: ff0000 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    
+    function animate() {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y -= 0.01;
+        renderer.render(scene, camera);
+    }
+    animate();
+
 }

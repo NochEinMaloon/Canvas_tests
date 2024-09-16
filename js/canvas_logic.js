255bf26316fa515cce2f1178d065756c9a777446
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-16 10:23:38 +0200

    add other cube

--- js/canvas_logic.js
+++ js/canvas_logic.js
@@ -67,6 +67,7 @@ onmousedown = (mousedownevent) => {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const otherCUbe = new THREE.Mesh(geometry, material);
+    scene.add(otherCUbe);
     
 
     function animate() {

e867037c5db04d7620e5f4686e6040f4de7bd731
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-25 10:56:28 +0200

    loadscene function

--- js/canvas_logic.js
+++ js/canvas_logic.js
@@ -41,6 +41,31 @@ function init(){
 }
 
 
+function loadscene(){
+    const loader = new THREE.ObjectLoader();
+
+    loader.load(
+        // resource URL
+        "Scenes/Scene.json",
+
+        // onLoad callback
+        // Here the loaded data is assumed to be an object
+        function ( obj ) {
+            // Add the loaded object to the scene
+            scene.add( obj );
+        },
+
+        // onProgress callback
+        function ( xhr ) {
+            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        },
+
+        // onError callback
+        function ( err ) {
+            console.error( 'An error happened' );
+        }
+    );
+}
 
 
 function Spawncube(){
